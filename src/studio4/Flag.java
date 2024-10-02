dee1dbcf401a3609b08be0f0e5b9d46b95533f1f
liaowang <liaowang@localhost>
2024-10-02 14:13:40 -0500

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,19 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.green);
+		StdDraw.filledRectangle(0.5, 0.5, 0.35, 0.2);
+		StdDraw.setPenColor(Color.red);
+		double [] x = {0.15, 0.5, 0.85, 0.5};
+		double [] y = {0.5, 0.7, 0.5, 0.3};
+		StdDraw.filledPolygon(x, y);
+		StdDraw.setPenColor(Color.cyan);
+		double [] m = {0.25, 0.5, 0.75, 0.5};
+		double [] n = {0.5, 0.65, 0.5, 0.35};
+		StdDraw.filledPolygon(m, n);
+		StdDraw.setPenColor(Color.yellow);
+		double [] p = {0.32, 0.5, 0.68, 0.5};
+		double [] q = {0.5, 0.6, 0.5, 0.4};
+		StdDraw.filledPolygon(p, q);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,52 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int c = in.nextInt();
+		Color pen = new Color(a,b,c);
+		StdDraw.setPenColor(pen);
+		boolean filled = in.nextBoolean();
+		if(shape.equals("rectangle")) {
+			double m = in.nextDouble();
+			double n = in.nextDouble();
+			double p = in.nextDouble();
+			double q = in.nextDouble();
+			if (filled == true) {
+				StdDraw.filledRectangle(m, n, p, q);
+			}
+			else {
+				StdDraw.rectangle(m, n, p, q);
+			}
+		}
+		else if(shape.equals("ellipse")) {
+			double m = in.nextDouble();
+			double n = in.nextDouble();
+			double p = in.nextDouble();
+			double q = in.nextDouble();
+			if (filled == true) {
+				StdDraw.filledEllipse(m, n, p, q);
+			}
+			else {
+				StdDraw.ellipse(m, n, p, q);
+			}
+		}
+		else if (shape.equals("triangle")) {
+			double m = in.nextDouble();
+			double n = in.nextDouble();
+			double p = in.nextDouble();
+			double q = in.nextDouble();
+			double u = in.nextDouble();
+			double v = in.nextDouble();
+			double []x= {m,n,p};
+			double []y = {q,u,v};
+			if (filled == true) {
+				StdDraw.filledPolygon(x, y);
+			}
+			else {
+				StdDraw.polygon(x, y);
+			}
+		}
 	}
 }
